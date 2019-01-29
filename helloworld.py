2e93052745b1586f0792776d4ed724fa2d35ebdd
gaoz@home <nnugaozhi@hotmail.com>
2019-01-29 22:18:01 +0800

    begin python with hellowork.py

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+print('''This is a multi-line string. This is the first line.\
+This is the second line.
+"What's your name?",I asked.
+He said "Bond,James Bond."
+''')
+print(R"Newlines are indicated by \n")
+
+print(u"世界,你好！")
+
+
